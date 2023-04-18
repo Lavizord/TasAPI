79b49d1a56d20aac08795d9c5ebb4d16bab4d0b2
Lavizord <63145101+Lavizord@users.noreply.github.com>
2023-04-18 20:04:03 +0100

    chore: Atualizado TODO e devnotes, pequeno refractor.

--- TasContext.cs
+++ TasContext.cs
@@ -10,17 +10,20 @@ namespace Entities.Models
         public TasDB(DbContextOptions<TasDB> options):base(options)
         {
         }
+        // TODO: Criar os Models iniciais para cada uma das tabelas no diagrama.
         // Aqui definimos as nossas entidades, que são as nossas tabelas.
         public DbSet<Scene> Scenes { get; set; }
         public DbSet<Choice> Choices { get; set; }
         public DbSet<SceneEffect> SceneEffects { get; set; }
- 
+
+        // TODO: Definir as relações restantes no diagrama da DB
         //  Aqui estamos a usar FluentAPI para definir as relações entre as tabelas.
         //  Gosto de usar aqui porque fica mais explicito, pelo menos na minha opinião,
         // mas é possível fazer o mesmo nos Models de Entidades diretamente... São escolhas.
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             DefineSceneChoide(modelBuilder);     
+            DefineSceneEffect(modelBuilder);
         }
 
         private void DefineSceneChoide(ModelBuilder modelBuilder)
@@ -36,7 +39,10 @@ namespace Entities.Models
                 .HasMany<Choice>(s => s.OwnChoices)
                 .WithOne(c => c.OwnScene)
                 .HasForeignKey(c => c.OwnSceneId);
-            
+        }
+
+        private void DefineSceneEffect(ModelBuilder modelBuilder)
+        {
             // One to One relationship
             modelBuilder.Entity<Scene>()
                 .HasOne<SceneEffect>(s => s.SceneEffect)
--- devnotes.md
+++ devnotes.md
@@ -1,13 +1,13 @@
 ## Configuração Inicials
 
 - Pontos a fazer:
-  1. ~~Minimal web api criada.~~
-  2. ~~Configurado Swagger.~~
-  3. ~~EF6 Setup (added following packages to project)~~
-     ~~Microsoft.EntityFrameworkCore~~
-     ~~Microsoft.EntityFrameworkCore.Tools~~
-     ~~Microsoft.EntityFrameworkCore.SqlServer~~
-  4. ~~Code first aproch tested with SqlServer~~
+  1. Limpar lista de TODS em Program.cs e TasContext.cs
+  2. Ter os POSTS de criação de entidades funcionais.
+  3. Migração das Fixtures iniciais para DB, usando endpoints acima. (Este ponto pode ser feito em paralelo ao 2).
+     A. Ter as Scenes importadas.
+     B. Ter os SceneEffects Importados.
+     C. Ter as Choices importadas.
+     D. Ter os Items importados.
 
 ## Sql Server Logins (because fuck security) :D
 
