11fcd454efa257a1d0c5cefb76bc9ef80d5cb52e
PedroNunes <pedro.nunes@comsoftweb.pt>
2023-04-26 13:03:25 +0100

    chore: criado AppBuilder para configurar serviços e fazer build da App, agora está a ler connection string do ficheiro AppSettings.

---
+++ AppBuilder.cs
@@ -0,0 +1,46 @@
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using ConfigurationPOCO;
+
+
+
+public static class AppBuilder
+{
+    // Retorna WebApp com serviços configurados.
+    // App deve depois ser configurada / adicionado endpoints e iniciada .Run()
+    public static WebApplication GetApp(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.Configure<DatabaseConfiguration>(
+            builder.Configuration.GetSection("DatabaseConfiguration")
+        );
+
+        // Setup database connection // TODO: Alterar para POCO?
+        var connectionString = builder.Configuration.GetConnectionString("SqlConnectionString");
+        
+        builder
+            .Services
+            .AddDbContext<TasDB>(opt => opt.UseSqlServer(connectionString));
+        
+
+        builder
+            .Services
+            .AddEndpointsApiExplorer();
+        
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo {
+            Title = "Take a Step API",
+            Description = "Taking the steps you love",
+            Version = "v1" });
+        });
+
+        var app = builder.Build();
+        return app;
+    }
+}
