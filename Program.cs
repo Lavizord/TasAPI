d054d2ee7132ac1f2646864ab21c1f4bfe8d3736
PedroNunes <pedro.s.nunes94@gmail.com>
2023-04-27 18:04:05 +0100

    feat: sql script para insert de dados na BD. Criada as Scenes da story 1.

--- Program.cs
+++ Program.cs
@@ -16,14 +16,6 @@ app.MapGet("/scenes", async (TasDB db) =>
     await db.Scenes.Include("SceneEffect").ToListAsync();
 });
 
-// TODO: Criar as scenas já existentes com este endpoint. Documentar particularidades.
-app.MapPost("/scenes", async (TasDB db, Scene scene) =>
-{
-    await db.Scenes.AddAsync(scene);
-    await db.SaveChangesAsync();
-    return Results.Created($"/scene/{scene._Id}", scene);
-});
-
 app.MapGet("/sceneseffect", async (TasDB db) =>
 {
     await db.SceneEffects.ToListAsync();
